2440aac4e559d4749386883a32fae51a55f3e0f8
Sumesh <sumeshmon2006@gmail.com>
2025-11-29 11:58:59 +0530

    react

---
+++ javascript-react-app/src/lessions/declareitivity.jsx
@@ -0,0 +1,3 @@
+export default function Declaritivity() {
+  return <h2>This iasds declarativity lesson</h2>;
+}
---
+++ my-react-app/src/main.tsx
@@ -0,0 +1,9 @@
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+createRoot(document.getElementById('root')!).render(
+  <ul>
+    <li>Test</li>
+  </ul>
+)
