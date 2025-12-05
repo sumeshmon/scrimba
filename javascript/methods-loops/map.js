1a76ff245e7a6b9db3f9b4b38301beb39570c5cb
Sumesh <sumeshmon2006@gmail.com>
2025-12-05 09:11:11 +0530

    map

--- javascript/methods-loops/map.js
+++ javascript/methods-loops/map.js
@@ -1,3 +1,11 @@
 const distanceWalkedMilesArr = [140, 153, 161, 153, 128, 148]
 
 const conversionFactorMilesToKm = 1.6
+
+function convertMilesToKms() {
+    return  distanceWalkedMilesArr.map(function (distanceMiles) {
+       return distanceMiles * conversionFactorMilesToKm
+    })
+   
+} 
+console.log(convertMilesToKms());
