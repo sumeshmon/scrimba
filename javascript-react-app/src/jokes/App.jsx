8d70a68ddffab3eec60924b41b247906ecfd6e47
Sumesh <sumeshmon2006@gmail.com>
2026-02-14 08:56:34 +0530

    joke toggle boolean

--- javascript-react-app/src/jokes/App.jsx
+++ javascript-react-app/src/jokes/App.jsx
@@ -1,5 +1,6 @@
 import Jokes from "./Jokes";
 import JokesData from "./JokesData";
+
 /**
  * Challenge: Think critically - how would you pass in a prop that 
  * isn't a string datatype.
@@ -12,19 +13,24 @@ import JokesData from "./JokesData";
 
 export default function App() {
     /**
-     * Challenge: See if you can correctly pass the necessary props to the 
+     * Challenge 1: See if you can correctly pass the necessary props to the 
      * Joke component in the .map() (and render the jokeElements array) so 
      * the jokes show up on the page again
      */
+
+   
+
     const jokeElements = JokesData.map((joke) => {
             return <Jokes 
+                key={joke.id}
                 setup = {joke.setup}
                 punchline = {joke.punchline}
+                
             />
     })
     return ( 
         <>
-            <main>
+            <main className="joke-section">
                 {jokeElements}
             </main>
         </>
--- javascript-react-app/src/jokes/Jokes.jsx
+++ javascript-react-app/src/jokes/Jokes.jsx
@@ -1,11 +1,22 @@
+import React  from "react";
 export default function Jokes(props) {
-    console.log(props);
-    
+    // console.log(props);
+     /**
+         * Challenge 2:
+         * - Create state `isShown` (boolean, default to `false`)
+         * - Add a button that toggles the value back and forth
+         */
+       
+     const [isShown, setIsShown] = React.useState(false)
+        function handlShow(){
+            setIsShown ( prevState => !prevState)
+        }
     return (
         <>
             <div className="joke">            
                 {props.setup && <div className="joke-title"><b>Setup:</b> {props.setup}</div>}
                 <div className="joke-title"><b>Punchline:</b> {props.punchline}</div>
+                <button onClick={handlShow}>{isShown ? 'Show' : 'hide'}</button>
             </div>
 
         </> 
