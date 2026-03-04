5849d7882c7896391d48fcbb43023bc5157c2dea
Sumesh <sumeshmon2006@gmail.com>
2026-03-04 11:02:26 +0530

    button click

--- react-project/tenzies/tenzies/src/App.jsx
+++ react-project/tenzies/tenzies/src/App.jsx
@@ -5,17 +5,38 @@ import './App.css'
 import Die from './Die'
 function App() {
 
-const [value, setValue] = useState(0)
+  const [value, setValue] = useState(0)
+
+  function generateAllNewDice() {
+   
+    return new Array(10)
+      .fill(0)
+      .map(() => Math.ceil(Math.random() * 6))
+
+    // or 
+    //   const arr = Array.from({ length: 10 }, () => {
+    //   return Math.floor(Math.random() * 6) + 1
+    // })
+  }
+
+  const [dice, setdice] = useState(generateAllNewDice())
+  const diceElements = dice.map(items =>
+    <Die
+      key={items.id}
+      value={items}
+    />
+  )
+
+  function rollDice(){
+    setdice(generateAllNewDice())
+  }
   return (
     <>
       <main>
-      <div className="dice-container">
-        <Die value={value}/>
-        <Die value={value}/>
-        <Die value={value}/>
-        <Die value={value}/>
-      </div>
-        
+        <div className="dice-container">
+          {diceElements}
+        </div>
+         <button onClick={rollDice}>Roll Dice</button>
       </main>
     </>
   )
