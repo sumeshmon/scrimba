eda99a657319268ec025b0f310d796799e3c0ba2
Sumesh <sumeshmon2006@gmail.com>
2026-06-30 16:55:15 +0530

    headers

--- node/server.js
+++ node/server.js
@@ -1,5 +1,5 @@
 import http from 'node:http'
-
+import { getDataFromDB } from './database/db.js'
 const PORT = 8000
 /*
 Challenge:
@@ -12,17 +12,24 @@ Challenge:
 1. Check the ‘method’ property on the req object.
    Only serve our string if it’s ‘GET’. 
 */
-const server = http.createServer((req, res) => {
-    // console.log(req.method);
-    
+
+/*
+Challenge:
+  1. Store our data in a const ‘destinations’.
+  2. When a GET request is received to the ‘/api' endpoint, send our JSON stringified data.
+    Think: What changes will you need to make to get this to work?
+*/
+const server = http.createServer(async (req, res) => {
+    const destinations = await getDataFromDB()
+    // console.log(destinations);
     if (req.url === '/api' && req.method === 'GET') {
-       res.end('This is from server')
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.end(JSON.stringify(destinations))
     }
 
-
-    
-
 })
+
 server.listen(PORT, () => {
     console.log(`server running on port: ${PORT}`);
 
