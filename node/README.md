63c7587096def28da322a82d36abe77bc37da4ac
Sumesh <sumeshmon2006@gmail.com>
2026-06-30 07:18:30 +0530

    response

--- node/README.md
+++ node/README.md
@@ -12,3 +12,15 @@ Contains metadata (name, version, author, description, etc.)<br>
 Simplifies collaboration<br>
 Manages dependencies<br>
 Defines the start script<br>
+
+# The HTTP Module
+Allows data to be transferred over the HTTP protocol.<br>
+Creates servers.<br>
+Handles requests from clients.<br>
+Provides responses to those requests.<br>
+
+# The response object
+Has methods which allow us to:<br>
+Specify the content type.<br>
+Set status codes (200, 400, etc.).<br>
+Provide content (HTML, JSON, images).<br>
--- node/server.js
+++ node/server.js
@@ -1,1 +1,29 @@
 import http from 'node:http'
+
+const PORT = 8000
+/*
+Challenge:
+Check the ‘url’ property on the req object. 
+Only serve our string if it’s ‘/api’.
+*/
+
+/*
+Challenge: 
+1. Check the ‘method’ property on the req object.
+   Only serve our string if it’s ‘GET’. 
+*/
+const server = http.createServer((req, res) => {
+    // console.log(req.method);
+    
+    if (req.url === '/api' && req.method === 'GET') {
+       res.end('This is from server')
+    }
+
+
+    
+
+})
+server.listen(PORT, () => {
+    console.log(`server running on port: ${PORT}`);
+
+})
